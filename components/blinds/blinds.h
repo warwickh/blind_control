172b9f764e914acda263a9ab99d350b8a07a95ec
Your Name <you@example.com>
2022-09-20 19:55:36 +1000

    initial config

--- components/blinds/blinds.h
+++ components/blinds/blinds.h
@@ -4,9 +4,9 @@
 #include "esphome/components/cover/cover.h"
 
 namespace esphome {
-namespace venetian_blinds {
+namespace blinds {
 
-class VenetianBlinds : public Component, public cover::Cover {
+class Blinds : public Component, public cover::Cover {
   public:
     void setup() override;
     void loop() override;
@@ -16,25 +16,15 @@ class VenetianBlinds : public Component, public cover::Cover {
     Trigger<> *get_open_trigger() const { return this->open_trigger; }
     Trigger<> *get_close_trigger() const { return this->close_trigger; }
     Trigger<> *get_stop_trigger() const { return this->stop_trigger; }    
-    void set_open_duration(uint32_t open) { this->open_duration = open; }
-    void set_close_duration(uint32_t close) { this->close_duration = close; }
-    void set_tilt_duration(uint32_t tilt) { this->tilt_duration = tilt; }
-    void set_assumed_state(bool value) { this->assumed_state = value; }
   private:
     uint32_t last_position_update{0};
-    uint32_t last_tilt_update{0};
     int exact_pos;
     int relative_pos{0};
-    int exact_tilt;
-    int relative_tilt{0};
     cover::CoverOperation current_action{cover::COVER_OPERATION_IDLE};
   protected:
     Trigger<> *open_trigger{new Trigger<>()};
     Trigger<> *close_trigger{new Trigger<>()};
     Trigger<> *stop_trigger{new Trigger<>()};
-    uint32_t open_duration;
-    uint32_t close_duration;
-    uint32_t tilt_duration;
     bool assumed_state{false};
 };
 
--- components/blinds/cover.py
+++ components/blinds/cover.py
@@ -5,27 +5,18 @@ from esphome.components import cover
 from esphome.const import (
     CONF_ID,
     CONF_CLOSE_ACTION,
-    CONF_CLOSE_DURATION,
     CONF_OPEN_ACTION,
-    CONF_OPEN_DURATION,
     CONF_STOP_ACTION,
-    CONF_ASSUMED_STATE,
 )
 
-CONF_TILT_DURATION = "tilt_duration"
-
-venetian_blinds_ns = cg.esphome_ns.namespace('venetian_blinds')
-VenetianBlinds = venetian_blinds_ns.class_('VenetianBlinds', cover.Cover, cg.Component)
+blinds_ns = cg.esphome_ns.namespace('blinds')
+Blinds = blinds_ns.class_('Blinds', cover.Cover, cg.Component)
 
 CONFIG_SCHEMA = cover.COVER_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(VenetianBlinds),
+    cv.GenerateID(): cv.declare_id(Blinds),
     cv.Required(CONF_OPEN_ACTION): automation.validate_automation(single=True),
-    cv.Required(CONF_OPEN_DURATION): cv.positive_time_period_milliseconds,
     cv.Required(CONF_CLOSE_ACTION): automation.validate_automation(single=True),
-    cv.Required(CONF_CLOSE_DURATION): cv.positive_time_period_milliseconds,
     cv.Required(CONF_STOP_ACTION): automation.validate_automation(single=True),
-    cv.Required(CONF_TILT_DURATION): cv.positive_time_period_milliseconds,
-    cv.Optional(CONF_ASSUMED_STATE, default=True): cv.boolean,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -35,14 +26,11 @@ async def to_code(config):
     await automation.build_automation(
         var.get_stop_trigger(), [], config[CONF_STOP_ACTION]
     )
-    cg.add(var.set_open_duration(config[CONF_OPEN_DURATION]))
     await automation.build_automation(
         var.get_open_trigger(), [], config[CONF_OPEN_ACTION]
     )
-    cg.add(var.set_close_duration(config[CONF_CLOSE_DURATION]))
     await automation.build_automation(
         var.get_close_trigger(), [], config[CONF_CLOSE_ACTION]
     )
-    cg.add(var.set_tilt_duration(config[CONF_TILT_DURATION]))
-    cg.add(var.set_assumed_state(config[CONF_ASSUMED_STATE]))
+
 
